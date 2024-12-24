7f61d483d09aad5811c5e21cfda4961568005e66
Peter Dinis <peterdinis0@pobox.sk>
2024-12-24 13:09:03 +0100

    infra: Working on clerk convex auth WIP

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
       {
-        domain: "https://your-issuer-url.clerk.accounts.dev/",
+        domain: "https://awaited-grub-13.clerk.accounts.dev/",
         applicationID: "convex",
       },
     ]
