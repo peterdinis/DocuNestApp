5751b932b6aef598a646f96a92ef2a5801e9c3d0
peterdinis <petter1@pobox.sk>
2024-12-28 17:27:57 +0100

    feat: Working on dashboard WIP

--- app/_components/dashboard/DashboardContent.tsx
+++ app/_components/dashboard/DashboardContent.tsx
@@ -1,11 +1,17 @@
 import { FC } from "react";
 import Header from "../shared/Header";
+import { format } from "date-fns";
 
 const DashboardContent: FC = () => {
+    const actualDateTime = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+
     return (
         <main className="flex-1 overflow-auto p-8">
             <div className="mx-auto max-w-4xl">
                 <Header text="Hi user" />
+                <span className="prose-p: prose mt-10 dark:text-sky-50">
+                    Actual date and time: {actualDateTime}
+                </span>
             </div>
         </main>
     )
