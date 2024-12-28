d68a3575e51c3f099c3abbc5a831eac34a8ea766
peterdinis <petter1@pobox.sk>
2024-12-28 17:23:12 +0100

    feat: Design dashboard

--- app/_components/dashboard/DashboardWrapper.tsx
+++ app/_components/dashboard/DashboardWrapper.tsx
@@ -1,11 +1,15 @@
 import { FC } from "react";
+import Sidebar from "../shared/Sidebar";
 
 const DashboardWrapper: FC = () => {
     return (
-        <>
-            Dashboard
-        </>
-    )
-}
+        <div className="dark:bg-dark dark:text-light flex min-h-screen">
+            <Sidebar />
+            <div className="flex-1 overflow-auto">
+                Content
+            </div>
+        </div>
+    );
+};
 
 export default DashboardWrapper;
