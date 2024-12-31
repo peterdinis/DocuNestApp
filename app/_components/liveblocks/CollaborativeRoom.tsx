29b9974efff346b1ce41e07fdff5139fa84d6b95
peterdinis <petter1@pobox.sk>
2024-12-31 18:00:05 +0100

    feat: Working on editor WIP

--- app/_components/liveblocks/CollaborativeRoom.tsx
+++ app/_components/liveblocks/CollaborativeRoom.tsx
@@ -9,6 +9,7 @@ import { updateDocument } from '@/app/_actions/roomActions';
 import Loading from '../shared/Loading';
 import { Pen } from 'lucide-react';
 import ShareModal from '../shared/ShareModal';
+import ActiveCollaborators from './ActiveCollaborators';
 
 const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
   const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
@@ -93,7 +94,7 @@ const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: Col
               {loading && <p className="text-sm text-gray-400">saving...</p>}
             </div>
             <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
-              Active Collaborators
+              <ActiveCollaborators />
 
               <ShareModal 
                 roomId={roomId}
