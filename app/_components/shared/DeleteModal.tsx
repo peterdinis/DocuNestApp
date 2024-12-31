06765c9540491dfcfde52126bf88785115769e1d
peterdinis <petter1@pobox.sk>
2024-12-31 17:38:07 +0100

    FOO

---
+++ app/_components/shared/DeleteModal.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { Trash } from "lucide-react";
+import Loading from "./Loading";
+import { Button } from "@/components/ui/button";
+import { deleteDocument } from "@/app/_actions/roomActions";
+
+export const DeleteModal = ({ roomId }: DeleteModalProps) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const deleteDocumentHandler = async () => {
+    setLoading(true);
+
+    try {
+      await deleteDocument(roomId);
+      setOpen(false);
+    } catch (error) {
+      console.log("Error notif:", error);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button className="min-w-9 rounded-xl bg-transparent p-2 transition-all">
+          <Trash className="w-12 h-12" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="shad-dialog">
+        <DialogHeader>
+          <DialogTitle>Delete document</DialogTitle>
+          <DialogDescription>
+            Are you sure you want to delete this document? This action cannot be
+            undone.
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter className="mt-5">
+          <DialogClose asChild className="w-full bg-dark-400 text-white">
+            Cancel
+          </DialogClose>
+
+          <Button
+            variant="destructive"
+            onClick={deleteDocumentHandler}
+            className="gradient-red w-full"
+          >
+            {loading ? <Loading /> : "Delete"}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
