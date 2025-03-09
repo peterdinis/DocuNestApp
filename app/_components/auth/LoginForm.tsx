66f5d1b40b07dfeefc274ce0d51480667a908d77
peterdinis <petter1@pobox.sk>
2025-03-09 17:31:01 +0100

    feat: Updating login form

--- app/_components/auth/LoginForm.tsx
+++ app/_components/auth/LoginForm.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import {
@@ -8,7 +10,6 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { signIn } from "@/lib/auth-client"
 import { useAuthState } from "@/app/_hooks/useAuthState"
 import { LoginSchema } from "@/app/_schemas/authSchemas"
@@ -16,94 +17,102 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useRouter } from "next/router"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form"
+import { toast } from "sonner"
 
 export function LoginForm({
-    className,
-    ...props
-  }: React.ComponentPropsWithoutRef<"div">) {
-    const router = useRouter()
-    const { error, success, loading, setSuccess, setError, setLoading, resetState } = useAuthState();
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  const router = useRouter()
+  const { loading, setLoading, resetState } = useAuthState();
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    }
+  })
 
-    const form = useForm<z.infer<typeof LoginSchema>>({
-        resolver: zodResolver(LoginSchema),
-        defaultValues: {
-            email: '',
-            password: '',
-        }
-    })
-
-    const onSubmit = async (values: z.infer<typeof LoginSchema>) => {
-        try {
-          await signIn.email({
-            email: values.email,
-            password: values.password
-          }, {
-            onResponse: () => {
-              setLoading(false)
-            },
-            onRequest: () => {
-              resetState()
-              setLoading(true)
-            },
-            onSuccess: (ctx) => {
-                router.replace('/dashboard')
-            },
-          });
-        } catch (error) {
-          console.log(error)
-        }
-      }
-
-    return (
-      <div className={cn("flex flex-col gap-6", className)} {...props}>
-        <Card>
-          <CardHeader>
-            <CardTitle className="text-2xl">Login</CardTitle>
-            <CardDescription>
-              Enter your email below to login to your account
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <div className="flex flex-col gap-6">
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <a
-                      href="#"
-                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </a>
-                  </div>
-                  <Input id="password" type="password" required />
-                </div>
-                <Button type="submit" className="w-full">
-                  Login
-                </Button>
-                <Button variant="outline" className="w-full">
-                  Login with Google
-                </Button>
-              </div>
-              <div className="mt-4 text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <a href="#" className="underline underline-offset-4">
-                  Sign up
-                </a>
-              </div>
-            </form>
-          </CardContent>
-        </Card>
-      </div>
-    )
+  const onSubmit = async (values: z.infer<typeof LoginSchema>) => {
+    try {
+      await signIn.email({
+        email: values.email,
+        password: values.password
+      }, {
+        onResponse: () => {
+          setLoading(false)
+        },
+        onRequest: () => {
+          resetState()
+          setLoading(true)
+        },
+        onSuccess: (ctx) => {
+          toast("Login was successfull", {
+            className: "bg-green-800 text-white font-bold text-xl"
+          })
+          router.replace('/dashboard')
+        },
+      });
+    } catch (error) {
+      console.log(error)
+    }
   }
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form className='space-y-4' onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        type="email"
+                        placeholder='example@gmail.com'
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        type="password"
+                        placeholder='********'
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+
+                )}
+              />
+              <Button disabled={loading} type="submit" className='w-full'>Login</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
