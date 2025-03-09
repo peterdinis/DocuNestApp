bb0ed9b5a8dc20abd0c3778d61ae93a6f056352e
peterdinis <petter1@pobox.sk>
2025-03-09 13:16:29 +0100

    feat: Create new auth pages

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const SignInPage: NextPage = () => {
+    return (
+        <>
+            ABCD
+        </>
+    )
+}
+
+export default SignInPage
---
+++ app/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const SignUpPage: NextPage = () => {
+    return (
+        <>
+            SignUp
+        </>
+    )
+}
+
+export default SignUpPage
