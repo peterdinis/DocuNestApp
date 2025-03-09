b0a7530afadf9d138c79e2d9f7c0f423db5dd8f0
peterdinis <petter1@pobox.sk>
2025-03-09 18:08:14 +0100

    infra: Start working on folders API WIP

--- app/api/folders/route.ts
+++ app/api/folders/route.ts
@@ -1,1 +1,17 @@
-TODO
+import { getSession } from '@/lib/auth-client';
+import { db } from '@/lib/db';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    const session = getSession()
+    const allUsersFolders = await db.folder.findMany({
+        where: {
+            userId: (await session).data?.user.id
+        },
+    });
+    if (!allUsersFolders) {
+        throw new Error('User does not create any folders');
+    }
+
+    return NextResponse.json(allUsersFolders);
+}
--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -11,5 +11,6 @@ export const {
     signIn,
     signOut,
     signUp,
+    getSession,
     useSession
 } = authClient;
