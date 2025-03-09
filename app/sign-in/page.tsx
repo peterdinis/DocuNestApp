cc28309e05cffb76bc9ed3702ae28502a0a87aa6
peterdinis <petter1@pobox.sk>
2025-03-09 13:36:08 +0100

    FOO

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -2,7 +2,13 @@ import { NextPage } from "next";
 import { LoginForm } from "../_components/auth/LoginForm";
 
 const SignInPage: NextPage = () => {
-    return <LoginForm />
+    return (
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                <LoginForm />
+            </div>
+        </div>
+    )
 }
 
 export default SignInPage
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -2,7 +2,13 @@ import { NextPage } from "next";
 import { RegisterForm } from "../_components/auth/RegisterForm";
 
 const SignUpPage: NextPage = () => {
-    return <RegisterForm />
+    return (
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                <RegisterForm />
+            </div>
+        </div>
+    )
 }
 
 export default SignUpPage
