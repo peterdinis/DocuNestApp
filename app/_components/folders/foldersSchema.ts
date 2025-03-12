d45521a8e97c1c31045e5162554ed4f9417741db
peterdinis <petter1@pobox.sk>
2025-03-12 18:19:27 +0100

    infra: Start working on folders logic

---
+++ app/_components/folders/foldersSchema.ts
@@ -0,0 +1,7 @@
+import { z } from 'zod';
+
+export const schema = z.object({
+    name: z.string().min(1, 'Folder name is required'),
+});
+
+export type FolderData = z.infer<typeof schema>;
---
+++ app/folders/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next"
+import DashboardLayout from "../_components/dashboard/DashboardLayout"
+
+const AllFoldersPage: NextPage = () => {
+    return (
+        <DashboardLayout>
+            Folders
+        </DashboardLayout>
+    )
+}
+
+export default AllFoldersPage
