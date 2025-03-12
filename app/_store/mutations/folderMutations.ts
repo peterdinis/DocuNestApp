25c5bdd99f4570de4b62965dd6aba2d21aa3f79d
peterdinis <petter1@pobox.sk>
2025-03-12 18:24:10 +0100

    feat: Setup tanstack-queries/mutations for folder logic

---
+++ app/_store/mutations/folderMutations.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export interface ICreateFolder {
+    id?: string;
+    name: string;
+}
+
+export type UpdateFolderData = Partial<ICreateFolder>;
+
+export interface IMoveToTrash {
+    folderId?: string;
+    documentId?: string;
+    inTrash: boolean;
+}
+
+export const createNewFolder = async (data: ICreateFolder) => {
+    return await axios.post('/api/folders/new', data);
+};
+
+export const updateFolder = async (
+    folderId: string,
+    data: UpdateFolderData,
+) => {
+    return await axios.put(`/api/folders/${folderId}`, data);
+};
+
+export const moveToTrashFolder = async (data: IMoveToTrash) => {
+    return await axios.put(`/api/folders/${data.folderId}/trash`, data);
+};
+
+export const removeFromTrashFolder = async (data: IMoveToTrash) => {
+    return await axios.put(`/api/folders/${data.folderId}/trash/remove`, data);
+};
