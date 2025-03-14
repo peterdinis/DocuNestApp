35be22df56784a78fc0b3237e8717617ea87721d
peterdinis <petter1@pobox.sk>
2025-03-14 18:08:39 +0100

    FOO

---
+++ app/_store/queries/documentQueries.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export const fetchAllPaginatedDocuments = async ({ query = '', page = 1 }) => {
+    const response = await axios.get('/api/docs/paginated', {
+        params: {
+            query,
+            page,
+        },
+    });
+    return response.data;
+};
+
+export const fetchAllTrashDocuments = async (page: number, limit: number) => {
+    const response = await axios.get('/api/docs/trash', {
+        params: { page, limit },
+    });
+    return response.data;
+};
+export const fetchAllDocuments = async () => {
+    const response = await axios.get('/api/docs');
+    return response.data;
+};
+
+export const fetchDocumentDetail = async (id: number | string) => {
+    const response = await axios.get(`/api/docs/${id}`);
+    if (!id) return;
+
+    return response.data;
+};
