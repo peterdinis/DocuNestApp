444d07921b8d00cb686534f8153384abdb2b44d1
peterdinis <petter1@pobox.sk>
2025-03-19 18:48:18 +0100

    feat: Create not-allowed page

---
+++ app/_components/shared/NotAllowedWrapper.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import Link from 'next/link';
+import { FC } from 'react';
+
+const NotAllowedWrapper: FC = () => {
+    return (
+        <div className='flex min-h-screen flex-col items-center justify-center'>
+            <div className='mt-20'>
+                <p className='text-4xl font-bold'>
+                    You must be logged in first
+                </p>
+            </div>
+            <button className='mt-8 rounded bg-blue-500 px-4 py-2 text-white'>
+                <Link className='text-xl' href='/login'>
+                    Login
+                </Link>
+            </button>
+        </div>
+    );
+};
+
+export default NotAllowedWrapper;
