d21314a2801bc099aaf6d6dec2a77fc3dc03e6b6
peterdinis <petter1@pobox.sk>
2025-03-29 20:07:28 +0100

    feat: Finish missing todo here for upload file

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,11 +1,12 @@
 import { getSession } from '@/lib/auth-client';
+import { db } from '@/lib/db';
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
 import { UploadThingError } from 'uploadthing/server';
 
 const f = createUploadthing();
 
 export const uploadRouter = {
-    fileUploader: f(['application/pdf', 'text/plain', 'application/msword'])
+    fileUploader: f(['application/pdf', "application/xml", 'text/plain', 'application/msword'])
         .middleware(async ({ req }) => {
             const session = await getSession()
             if (!session || !session.data!.user) {
@@ -15,7 +16,16 @@ export const uploadRouter = {
         })
         .onUploadComplete(async ({ metadata, file }) => {
             try {
-                // TODO: File opearion for add
+                const session = await getSession()
+                const appFile = await db.file.create({
+                    data: {
+                        name: file.name,
+                        size: file.size,
+                        createdAt: new Date().toISOString(),
+                        type: file.type,
+                        userId:  session.data?.user.id!,
+                    }
+                })
                 return { uploadedBy: metadata.userId };
             } catch (error) {
                 console.error('Error in onUploadComplete:', error);
